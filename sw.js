2da6bae0a8dd99364896d7080666e76d862e69db
alexgomez12 <86858528+alexgomez12@users.noreply.github.com>
2026-03-11 17:01:35 -0300

    Update service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'mr-v2';
+var CACHE = 'mr-v3';
 var ARCHIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
